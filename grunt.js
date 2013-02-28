1a687c7f2a5532f656b2258b7ef6a83409510388
Todd Wolfson <todd@twolfson.com>
2013-02-28 03:44:55 -0800

    Corrected lint errors

--- grunt.js
+++ grunt.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     lint: {
-      files: ['grunt.js', 'tasks/**/*.js', 'test/**/*.js']
+      files: ['grunt.js', 'tasks/**/*.js', 'test/*.js']
     },
     watch: {
       files: '<config:lint.files>',
@@ -29,9 +29,6 @@ module.exports = function(grunt) {
       globals: {}
     }
   });
-
-  // Load local tasks.
-  grunt.loadTasks('tasks');
 
   // Default task.
   grunt.registerTask('default', 'lint');
