03fc7d429239f167b6556d889ddc5d91ab47368b
Todd Wolfson <todd@twolfson.com>
2013-03-03 21:42:06 -0800

    Attempting to deal with dynamic dest but it is getting tricky -- this is not cross version x_x

--- test/grunt.js
+++ test/grunt.js
@@ -3,30 +3,31 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: require('../package.json'),
+    // pkg: '<json:package.json>',
     zip: {
-      single: {
-        src: ['test_files/file.js'],
-        dest: 'actual/single_zip/file.zip'
-      },
-      multi: {
-        src: ['test_files/file.js', 'test_files/file2.js'],
-        dest: 'actual/multi_zip/file.zip'
-      },
+      // single: {
+      //   src: ['test_files/file.js'],
+      //   dest: 'actual/single_zip/file.zip'
+      // },
+      // multi: {
+      //   src: ['test_files/file.js', 'test_files/file2.js'],
+      //   dest: 'actual/multi_zip/file.zip'
+      // },
       // Template in dest test
       'actual/template_zip/<%= pkg.name %>.zip': ['test_files/file.js']
     },
     unzip: {
-      simple: {
-        src: 'test_files/file.zip',
-        dest: 'actual/unzip'
-      },
-      nested: {
-        src: 'test_files/nested.zip',
-        dest: 'actual/nested'
-      }
+      // simple: {
+      //   src: 'test_files/file.zip',
+      //   dest: 'actual/unzip'
+      // },
+      // nested: {
+      //   src: 'test_files/nested.zip',
+      //   dest: 'actual/nested'
+      // }
     },
     test: {
-      all: '*_test.js'
+      // all: '*_test.js'
     }
   });
 
