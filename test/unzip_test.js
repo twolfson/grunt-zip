796922d2a977dd85633810b40383247915bc0748
Todd Wolfson <todd@twolfson.com>
2014-06-14 15:27:43 -0700

    Completed another test conversion

--- test/unzip_test.js
+++ test/unzip_test.js
@@ -16,24 +16,23 @@ describe('A grunt `unzip` task', function () {
       fsUtils.assertEqualFiles('single_unzip/b.js');
     });
   });
+
+  describe('unzipping a file with nested folders', function () {
+    gruntUtils.runTask('unzip:nested');
+
+    it('extracts all expected files', function () {
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/css/bootstrap-responsive.css');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/css/bootstrap-responsive.min.css');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/css/bootstrap.css');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/css/bootstrap.min.css');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/img/glyphicons-halflings-white.png');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/img/glyphicons-halflings.png');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/js/bootstrap.js');
+      fsUtils.assertEqualFiles('nested_unzip/bootstrap/js/bootstrap.min.js');
+    });
+  });
 });
+
+// TODO: Remove me
 exports.world = {
-  'nestedUnzip': function (test) {
-    test.expect(8);
-    addMethods(test);
-
-    // unzip:nested
-
-    // Compare all nested unzip files
-    test.equalFiles('nested_unzip/bootstrap/css/bootstrap-responsive.css');
-    test.equalFiles('nested_unzip/bootstrap/css/bootstrap-responsive.min.css');
-    test.equalFiles('nested_unzip/bootstrap/css/bootstrap.css');
-    test.equalFiles('nested_unzip/bootstrap/css/bootstrap.min.css');
-    test.equalFiles('nested_unzip/bootstrap/img/glyphicons-halflings-white.png');
-    test.equalFiles('nested_unzip/bootstrap/img/glyphicons-halflings.png');
-    test.equalFiles('nested_unzip/bootstrap/js/bootstrap.js');
-    test.equalFiles('nested_unzip/bootstrap/js/bootstrap.min.js');
-
-    test.done();
-  }
 };
