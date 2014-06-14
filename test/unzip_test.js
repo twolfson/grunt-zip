3c076c87dd2c046c7b10679c4542a0592b33a703
Todd Wolfson <todd@twolfson.com>
2014-06-14 15:47:50 -0700

    Moved over unzip tests

--- test/unzip_test.js
+++ test/unzip_test.js
@@ -1,4 +1,5 @@
 // Load in dependencies
+var fs = require('fs');
 var expect = require('chai').expect;
 var fsUtils = require('./utils/fs');
 var gruntUtils = require('./utils/grunt');
@@ -31,53 +32,46 @@ describe('A grunt `unzip` task', function () {
       fsUtils.assertEqualFiles('nested_unzip/bootstrap/js/bootstrap.min.js');
     });
   });
+
+  describe('unzipping a file with a `router` parameter', function () {
+    gruntUtils.runTask('unzip:router');
+
+    it('maps the files to their new locations', function () {
+      fsUtils.assertEqualFiles('router_unzip/bootstrap-responsive.css');
+      fsUtils.assertEqualFiles('router_unzip/bootstrap-responsive.min.css');
+      fsUtils.assertEqualFiles('router_unzip/bootstrap.css');
+      fsUtils.assertEqualFiles('router_unzip/bootstrap.min.css');
+      fsUtils.assertEqualFiles('router_unzip/glyphicons-halflings-white.png');
+      fsUtils.assertEqualFiles('router_unzip/glyphicons-halflings.png');
+      fsUtils.assertEqualFiles('router_unzip/bootstrap.js');
+      fsUtils.assertEqualFiles('router_unzip/bootstrap.min.js');
+    });
+  });
+
+  describe('unzipping a file with a `router` that skips files', function () {
+    gruntUtils.runTask('unzip:skip-files');
+
+    it('creates un-skipped files', function () {
+      fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/img/glyphicons-halflings-white.png');
+      fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/img/glyphicons-halflings.png');
+      fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/js/bootstrap.js');
+      fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/js/bootstrap.min.js');
+    });
+
+    it('does not create skipped files', function () {
+      fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap-responsive.css');
+      fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap-responsive.min.css');
+      fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap.css');
+      fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap.min.css');
+    });
+  });
+
+  describe('unzipping a file with nested empty directories', function () {
+    gruntUtils.runTask('unzip:empty');
+
+    it('creates the nested empty directories', function () {
+      var stats = fs.statSync('actual/empty/double_empty');
+      expect(stats.isDirectory()).to.equal(true);
+    });
+  });
 });
-
-// TODO: Remove me
-exports.world = {
-  'routerUnzip': function (test) {
-    test.expect(8);
-    addMethods(test);
-
-    // unzip:router
-
-    // Compare all router unzip files
-    fsUtils.assertEqualFiles('router_unzip/bootstrap-responsive.css');
-    fsUtils.assertEqualFiles('router_unzip/bootstrap-responsive.min.css');
-    fsUtils.assertEqualFiles('router_unzip/bootstrap.css');
-    fsUtils.assertEqualFiles('router_unzip/bootstrap.min.css');
-    fsUtils.assertEqualFiles('router_unzip/glyphicons-halflings-white.png');
-    fsUtils.assertEqualFiles('router_unzip/glyphicons-halflings.png');
-    fsUtils.assertEqualFiles('router_unzip/bootstrap.js');
-    fsUtils.assertEqualFiles('router_unzip/bootstrap.min.js');
-
-    test.done();
-  },
-  'skipFilesUnzip': function (test) {
-    test.expect(8);
-    addMethods(test);
-
-    // unzip:skip-files
-
-    // Assert CSS files do not exist
-    fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap-responsive.css');
-    fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap-responsive.min.css');
-    fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap.css');
-    fsUtils.assertNoFile('skip_files_unzip/bootstrap/css/bootstrap.min.css');
-
-    // Assert other files do exist
-    fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/img/glyphicons-halflings-white.png');
-    fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/img/glyphicons-halflings.png');
-    fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/js/bootstrap.js');
-    fsUtils.assertEqualFiles('skip_files_unzip/bootstrap/js/bootstrap.min.js');
-
-    test.done();
-  },
-  'emptyUnzip': function (test) {
-    // unzip:empty
-
-    var stats = fs.statSync('actual/empty/double_empty');
-    test.strictEqual(stats.isDirectory(), true);
-    test.done();
-  }
-};
