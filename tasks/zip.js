2f852bbec4cfe4ebc65cf83a5e5cea49c324adb5
Anmol Godiyal <anmol.godiyal@icanbwell.com>
2023-09-25 19:57:07 +0530

    ATHD-6111 migrate jszip to 3.10.1 from 2.7.0

--- tasks/zip.js
+++ tasks/zip.js
@@ -6,11 +6,12 @@
  * Licensed under the MIT license.
  */
 
-var fs = require('fs'),
-    path = require('path'),
-    Zip = require('jszip'),
-    gruntRetro = require('grunt-retro');
-module.exports = function(grunt) {
+const fs = require("fs"),
+  path = require("path"),
+  Zip = require("jszip"),
+  gruntRetro = require("grunt-retro");
+
+module.exports = function (grunt) {
   // Load and bind grunt-retro
   grunt = gruntRetro(grunt);
 
@@ -22,48 +23,50 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   // Localize underscore
-  var _ = grunt.utils._;
+  const _ = grunt.utils._;
 
-  grunt.registerMultiTask('zip', 'Zip files together', function() {
+  grunt.registerMultiTask("zip", "Zip files together", async function () {
+    // Async task
+    const done = this.async();
     // Localize variables
-    var file = this.file,
-        data = this.data,
-        src = file.src,
-        dest = file.dest,
-        router = data.router;
+    const file = this.file,
+      data = this.data,
+      src = file.src,
+      dest = file.dest;
+    let router = data.router;
 
     // Fallback options (e.g. base64, compression)
     _.defaults(data, {
-      base64: false
+      base64: false,
     });
 
     // Collect our file paths
-    var globOptions = {dot: data.dot},
-        srcFolders = grunt.file.expandDirs(globOptions, src),
-        srcFiles = grunt.file.expandFiles(globOptions, src);
+    const globOptions = { dot: data.dot },
+      srcFolders = grunt.file.expandDirs(globOptions, src),
+      srcFiles = grunt.file.expandFiles(globOptions, src);
 
     // If there is no router
     if (!router) {
       // Grab the cwd and return the relative path as our router
-      var cwd = data.cwd || process.cwd(),
-          separator = new RegExp(path.sep.replace('\\', '\\\\'), 'g');
-      router = function routerFn (filepath) {
+      const cwd = data.cwd || process.cwd(),
+        separator = new RegExp(path.sep.replace("\\", "\\\\"), "g");
+      router = function routerFn(filepath) {
         // Join path via /
         // DEV: Files zipped on Windows need to use /  to have the same layout on Linux
-        return path.relative(cwd, filepath).replace(separator, '/');
+        return path.relative(cwd, filepath).replace(separator, "/");
       };
     } else if (data.cwd) {
-    // Otherwise, if a `cwd` was specified, throw a fit and leave
-      grunt.fail.warn('grunt-zip does not accept `cwd` and `router` in the same config due to potential ordering complications. Please choose one.');
+      // Otherwise, if a `cwd` was specified, throw a fit and leave
+      grunt.fail.warn("grunt-zip does not accept `cwd` and `router` in the same config due to potential ordering complications. Please choose one.");
     }
 
     // Generate our zipper
-    var zip = new Zip();
+    const zip = new Zip();
 
     // For each of the srcFolders
     srcFolders.forEach(function (folderpath) {
       // Route the folder
-      var routedPath = router(folderpath);
+      const routedPath = router(folderpath);
 
       // If there is a folder, add it to the zip (allows for skipping)
       if (routedPath) {
@@ -75,8 +78,8 @@ module.exports = function(grunt) {
     // For each of the srcFiles
     srcFiles.forEach(function (filepath) {
       // Read in the content and add it to the zip
-      var input = fs.readFileSync(filepath),
-          routedPath = router(filepath);
+      const input = fs.readFileSync(filepath),
+        routedPath = router(filepath);
 
       // If it has a path, add it (allows for skipping)
       if (routedPath) {
@@ -86,106 +89,109 @@ module.exports = function(grunt) {
     });
 
     // Create the destination directory
-    var destDir = path.dirname(dest);
+    const destDir = path.dirname(dest);
     grunt.file.mkdir(destDir);
 
     // Write out the content
-    var output = zip.generate({type: 'nodebuffer', compression: data.compression});
+    const output = await zip.generateAsync({ type: "nodebuffer", compression: data.compression });
     fs.writeFileSync(dest, output);
 
     // Fail task if errors were logged.
-    if (this.errorCount) { return false; }
+    if (this.errorCount) {
+      return false;
+    }
 
     // Otherwise, print a success message.
     grunt.log.writeln('File "' + dest + '" created.');
+    // Mark task as completed now
+    done();
   });
 
   function echo(a) {
     return a;
   }
-  grunt.registerMultiTask('unzip', 'Unzip files into a folder', function() {
+  grunt.registerMultiTask("unzip", "Unzip files into a folder", async function () {
+    // Async task
+    const done = this.async();
     // Collect the filepaths we need
-    var file = this.file,
-        data = this.data,
-        src = file.src,
-        srcFiles = grunt.file.expand(src),
-        dest = file.dest,
-        router = data.router || echo;
+    const file = this.file,
+      data = this.data,
+      src = file.src,
+      srcFiles = grunt.file.expand(src),
+      dest = file.dest,
+      router = data.router || echo;
 
     // Fallback options (e.g. checkCRC32)
     _.defaults(data, {
       base64: false,
-      checkCRC32: true
+      checkCRC32: true,
     });
 
+    let filesWritten = false;
     // Iterate over the srcFiles
-    var filesWritten = false;
-    srcFiles.forEach(function (filepath) {
-      // Read in the contents
-      var input = fs.readFileSync(filepath);
+    const srcFilesPromises = srcFiles.map(async function (filepath) {
+      const input = fs.readFileSync(filepath);
 
       // Unzip it
-      var zip = new Zip(input, {checkCRC32: data.checkCRC32});
+      const zip = new Zip();
 
-      // Pluck out the files
-      var files = zip.files,
-          filenames = Object.getOwnPropertyNames(files);
+      const unzipped = await zip.loadAsync(input, { checkCRC32: data.checkCRC32 });
 
-      // Iterate over the files
-      filenames.forEach(function (filename) {
-        // Find the content
-        var fileObj = files[filename],
-            content = fileObj.asNodeBuffer(),
-            routedName = router(filename);
+      const unzippedPromises = Object.keys(unzipped.files).map(async function (filename) {
+        const content = await unzipped.files[filename].async("nodebuffer");
+        const fileObj = unzipped.files[filename];
+        const routedName = router(filename);
+        const filepath = path.join(dest, routedName);
+        filesWritten = true;
 
-        // If there is a file path (allows for skipping)
         if (routedName) {
-          // Determine the filepath
-          var filepath = path.join(dest, routedName);
-          filesWritten = true;
-
           // If the routedName ends in a `/`, treat it as a/an (empty) directory
           // DEV: We use `/` over path.sep since it is consistently `/` across all platforms
-          if (routedName.slice(-1) === '/') {
+          if (routedName.slice(-1) === "/") {
             grunt.verbose.writeln('Creating directory: "' + filepath + '"');
             grunt.file.mkdir(filepath);
           } else {
             // Create the destination directory
-            var fileDir = path.dirname(filepath);
+            const fileDir = path.dirname(filepath);
 
             // Write out the content
             grunt.file.mkdir(fileDir);
             if ((fileObj.unixPermissions & 0xf000) === 0xa000) {
-              var target = content.toString('utf8');
+              const target = content.toString("utf8");
               grunt.verbose.writeln('Creating symbolic link from: "' + filepath + '" to "' + target + '"');
               // fs.symlinkSync throws EEXIST if a file with the same name as the link already exists
               try {
                 fs.unlinkSync(filepath);
-              }
-              catch (err) {
-                if (err.code !== 'ENOENT') {
+              } catch (err) {
+                if (err.code !== "ENOENT") {
                   throw err;
                 }
               }
               fs.symlinkSync(target, filepath);
             } else {
               grunt.verbose.writeln('Writing file: "' + filepath + '"');
-              fs.writeFileSync(filepath, content, {mode: fileObj.unixPermissions});
+              fs.writeFileSync(filepath, content, { mode: fileObj.unixPermissions });
             }
           }
         }
       });
+
+      await Promise.all(unzippedPromises);
     });
 
-    // Fail task if errors were logged.
-    if (this.errorCount) { return false; }
+    await Promise.allSettled(srcFilesPromises);
+
+    if (this.errorCount) {
+      return false;
+    }
 
     // Otherwise, print a success message.
     if (filesWritten) {
-      grunt.log.writeln('Created "' + this.file.dest + '" directory');
+      grunt.log.writeln('Created "' + file.dest + '" directory');
     } else {
-      grunt.log.writeln('No files were found in source. "' + this.file.dest + '" has not been created.');
+      grunt.log.writeln('No files were found in source. "' + file.dest + '" has not been created.');
     }
+    // Mark task as completed now
+    done();
   });
-
 };
